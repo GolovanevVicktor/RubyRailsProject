08b036d33e54508851f9ecc634f4206e3b7a54b2
Artem Shlemko <artemshlemko01@gmail.com>
2017-06-30 10:58:27 +0300

    fixed template for Full_describtion_page (pageh.html.erb)

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170629145604) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
