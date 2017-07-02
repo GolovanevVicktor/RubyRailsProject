716a24a47b3386c5000a5712850ca773aa094b84
Artem Shlemko <artemshlemko01@gmail.com>
2017-07-02 16:04:13 +0300

    added: main page --> bg-image, greeting text, slogan text

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,15 +1,15 @@
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("go_top_btn").style.display = "block";
-        //document.getElementById("top_menu").style.display = "block";
-    } else {
-        document.getElementById("go_top_btn").style.display = "none";
-        //document.getElementById("top_menu").style.display = "none";
+    function scrollFunction() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            document.getElementById("go_top_btn").style.display = "block";
+            //document.getElementById("top_menu").style.display = "block";
+        } else {
+            document.getElementById("go_top_btn").style.display = "none";
+            //document.getElementById("top_menu").style.display = "none";
+        }
     }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
